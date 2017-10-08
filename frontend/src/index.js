567f26c95074da202911a17fa1b1e8e06b8e8255
Priscilla Macalincag <priscilla@optimalam.com>
2017-10-08 15:55:10 +1300

    feature: redux devtools added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,10 +10,8 @@ import reducer from './reducers';
 
 const store = createStore(
   reducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
-console.log(store.getState());
 
 ReactDOM.render(
   // <Provider store={store}>
