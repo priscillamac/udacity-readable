73310bc205e673d6fbebae347d89ebe7f88dcb02
Priscilla Macalincag <priscilla@optimalam.com>
2017-10-10 21:48:35 +1300

    redux thunk and axios installed. redux thunk implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { compose, applyMiddleware, createStore } from 'redux';
 import reducer from './reducers';
+import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(
+      ReduxThunk
+    ),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
